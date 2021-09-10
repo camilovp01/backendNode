b006b8eaf74fb076e0be9803d885ee76b83fa2e3
CamiloVelasquezP <cristian.velasquez@neoris.com>
2021-09-10 12:21:59 -0500

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 //Cors
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://adminhospital.azurewebsites.net/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTION");
     next();
